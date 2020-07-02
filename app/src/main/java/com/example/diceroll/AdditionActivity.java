b2110318684242c7595280f62d61c26eeeda0425
Elango <elango.om@gmail.com>
2020-07-02 21:42:48 +0530

    Jul 2 Completed Upto rating bar

--- app/src/main/java/com/example/diceroll/AdditionActivity.java
+++ app/src/main/java/com/example/diceroll/AdditionActivity.java
@@ -4,18 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class AdditionActivity extends AppCompatActivity {
 
-    Button bRollButton ;
-    TextView tTextView1,tGreenTextView,tYellowTextView,tAnsTextView;
+    Button bRollButton,bCheckAnswer,bEndGame ;
+    TextView tTextView1,tGreenTextView,tYellowTextView,tAnsTextView,tNumberofQuestions;
     ImageView iRedDice,iGreenDice,iYellowDice;
     EditText eResultNumber;
+    RatingBar rtScoreBar;
     double iReddicenumber =0;
     double iGreendicenumber =0;
     double iYellowdicenumber =0;
@@ -23,6 +26,10 @@ public class AdditionActivity extends AppCompatActivity {
     double dEnteredResult = 0;
     String sAction = "";
     String sLabel = "";
+
+    int iQ = 1;
+    int iCorrect = 0;
+    String siQ = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,30 +40,63 @@ public class AdditionActivity extends AppCompatActivity {
         setTitle(sLabel);
         setContentView(R.layout.activity_addition);
 
-
         bRollButton = findViewById(R.id.rollbutton);
+        bCheckAnswer = findViewById(R.id.checkanwer);
+        bEndGame =  findViewById(R.id.endgame);
+        bCheckAnswer.setEnabled(false);
         // tTextView1 = findViewById(R.id.resulttextView);
         //tGreenTextView = findViewById(R.id.greenresulttextView);
         //tYellowTextView = findViewById(R.id.yellowresulttextView);
         tAnsTextView = findViewById(R.id.answerstatus);
-
+        tNumberofQuestions = findViewById(R.id.numberofquestions);
         iRedDice = findViewById(R.id.redimageView);
         iGreenDice = findViewById(R.id.greenimageView);
         iYellowDice = findViewById(R.id.yellowimageView);
-
         eResultNumber = findViewById(R.id.signedresultnumber);
+        rtScoreBar = findViewById(R.id.ratingBar);
+
         bRollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 roll();
+            }
+        });
+
+        bEndGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                endgame();
             }
         });
     }
 
+    private void endgame() {
+
+        float d = (Float.valueOf(iCorrect) / Float.valueOf(iQ))* 100 ;
+        if(d>75) {
+            d = 5;
+        } else if(d>50 && d<75) {
+            d=3;
+        } else if(d>25 && d<50) {
+            d=2;
+        } else if(d>0 && d<25) {
+            d=1;
+        }
+        rtScoreBar.setRating(d);
+        bRollButton.setEnabled(false);
+        rtScoreBar.setVisibility(View.VISIBLE);
+
+    }
 
     private void roll() {
+        eResultNumber.setEnabled(true);
+        bEndGame.setEnabled(false);
         tAnsTextView.setText("");
         eResultNumber.setText("");
+        siQ = String.valueOf(iQ);
+        tNumberofQuestions.setText(siQ);
+
         int rollnumber = (int)(Math.random() * 6 + 1);
         switch (rollnumber) {
             case 1:
@@ -178,6 +218,9 @@ public class AdditionActivity extends AppCompatActivity {
                 break;
         }
 
+        bRollButton.setEnabled(false);
+        bCheckAnswer.setEnabled(true);
+        iQ++;
     }
 
     public void addnumber(View v) {
@@ -199,11 +242,18 @@ public class AdditionActivity extends AppCompatActivity {
             dEnteredResult = Double.parseDouble(eResultNumber.getText().toString());
 
             if (iresultnumber == dEnteredResult) {
+                iCorrect++;
                 tAnsTextView.setText("Correct Answer");
             } else {
                 tAnsTextView.setText("Wrong Answer");
             }
+            bRollButton.setEnabled(true);
+            eResultNumber.setEnabled(false);
+            bCheckAnswer.setEnabled(false);
+            bEndGame.setEnabled(true);
         }
 
     }
+
+
 }
